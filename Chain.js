6651c299d1c6d99cfaf3aa3e9ed50bff173a7ee2
Ruqaiya72 <103035548+Ruqaiya72@users.noreply.github.com>
2023-04-07 16:44:22 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,19 @@
+class Chain {
+constructor (bodyA,bodyB){
+var options={
+    bodyA:bodyA,
+    bodyB:bodyB,
+    stiffness:0.04,
+    length:10
+
+  }
+  this.chain=Constraint.create(options)
+  World.add(world,this.chain)
+
+
+}
+display(){
+    strokeWeight (3);
+    line (bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y)
+}
+}
